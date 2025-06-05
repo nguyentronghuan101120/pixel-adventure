246a893a67ed17a665eb1a5f521f328dbd3d0e45
LeoNguyen <nguyentronghuan101120>
2025-06-05 22:10:33 +0700

    update

--- assets/tiles/Boxes.tsx
+++ assets/tiles/Boxes.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Boxes" tilewidth="20" tileheight="20" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="boxes" tilewidth="20" tileheight="20" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image source="../images/custom/box1.png" width="20" height="20"/>
